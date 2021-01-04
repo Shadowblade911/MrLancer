41fa42655e81bd9e8d383b3702cd633732209d73
David Pogin <dpogin@gmail.com>
2021-01-03 23:39:31 -0600

    Adding help text

--- index.js
+++ index.js
@@ -38,6 +38,42 @@ const commands = {
 };
 
 
+const helpText1 = [
+    "Greetings, I'm MrLancer, as you have probably guessed by calling the help command. ",
+    "I respond to !MrLancer or !MrL if you want to be brief ",
+    "My job, other than being an English teacher, is to help record, track, and suggest fiction sources, authors and prompts for writers. "
+].join('\n');
+
+const helpText2 = [
+    "To recommend a fic, begin with `!MrLancer rec` and follow it with the name of the fic, and any other options that would apply.",
+    "For example: ",
+    "```!MrLancer rec `Talent Night` https://archiveofourown.org/works/26225452/chapters/63828814 complete no genre humor shipping multi-chapter pairings Danny/Ember``` ",
+    "Would recommend the fic Talent Night at the given url, mark it as incomplete, that it's genre's include shipping and humor, is a multi chapter fic, and it's pairings include Danny and Ember ",
+    "Another example would be... ",
+    "```!MrLancer rec `Just Fourteen` https://www.fanfiction.net/s/13680095/1/Just-Fourteen complete no genre angst world-building hurt-comfort multi-chapter``` ",
+    "and as a third example... ",
+    "```!MrLancer rec `Eidolon Interloper` https://archiveofourown.org/works/24452716/chapters/59008513 complete no genre x-over``` ",
+    "The minimum is that you must name the fic, and give the URL so that I can figure out how to group the fic reccomendations. Note the use of the back tick to contain spaces in titles. ",
+    "the search command. `!MrLancer search` will return a fiction that matches the parameters given. No parameters results in a random fic from the list. ",
+    "Meanwhile... ",
+    " ```!MrLancer search genre angst``` ",
+    "Returns a random angst ridden fic, as recommended by users such as yourself. ",
+    "```!MrLancer search name \`Smells Like Team Spirit\` ```",
+    "Will return a fic with the given name if it has been recommended.",
+].join('\n');
+
+const helpText3 = [
+    "`!MrLancer author` is used to sign up an an author as a prestigious member of our lists.",
+    "You use it like so...",
+    "```!MrLancer author `sign up` HeroineofTime https://archiveofourown.org/users/HeroineofTime/ ```",
+    "If you cross post to two sites, include the second url as a second parameter.",
+    "```!MrLancer author search Hazama_d20 ```",
+    "Will return your searched author if he has signed up.",
+    "```!MrLancer suggest Vlad gets a clue```",
+    "Will put the suggestion into a prompt list.",
+    "```!MrLancer prompt``` will return a random prompt from the list."    
+].join("\n");
+
 
 
 // this is how we start listening to a message
@@ -72,7 +108,9 @@ client.on("message", function (message) {
             fetch(message, strings);
             return;
         case commands.help:
-            message.reply("this should eventually become a useful help message");
+            message.reply(helpText1);
+            message.channel.send(helpText2);
+            message.channel.send(helpText3);
             return;
         case commands.fetch:
             // I have to include some jokes for my own sanity. 
