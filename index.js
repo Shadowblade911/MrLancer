d205c36e15c0a7ef9c7221d393a27c06c5951d36
David Pogin <dpogin@gmail.com>
2021-01-04 00:23:45 -0600

    Updating help message + proper response to old man.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const commands = {
 
 const helpText1 = [
     "Greetings, I'm MrLancer, as you have probably guessed by calling the help command. ",
-    "I respond to !MrLancer or !MrL if you want to be brief ",
+    "I respond to !MrLancer or !MrL if you want to be brief. ",
     "My job, other than being an English teacher, is to help record, track, and suggest fiction sources, authors and prompts for writers. "
 ].join('\n');
 
@@ -48,13 +48,13 @@ const helpText2 = [
     "To recommend a fic, begin with `!MrLancer rec` and follow it with the name of the fic, and any other options that would apply.",
     "For example: ",
     "```!MrLancer rec `Talent Night` https://archiveofourown.org/works/26225452/chapters/63828814 complete no genre humor shipping multi-chapter pairings Danny/Ember``` ",
-    "Would recommend the fic Talent Night at the given url, mark it as incomplete, that it's genre's include shipping and humor, is a multi chapter fic, and it's pairings include Danny and Ember ",
-    "Another example would be... ",
+    "Would recommend the fic Talent Night at the given url, mark it as incomplete, that its genres include shipping and humor, is a multi chapter fic, and its pairings include Danny and Ember ",
+    "Another example is... ",
     "```!MrLancer rec `Just Fourteen` https://www.fanfiction.net/s/13680095/1/Just-Fourteen complete no genre angst world-building hurt-comfort multi-chapter``` ",
     "and as a third example... ",
     "```!MrLancer rec `Eidolon Interloper` https://archiveofourown.org/works/24452716/chapters/59008513 complete no genre x-over``` ",
-    "The minimum is that you must name the fic, and give the URL so that I can figure out how to group the fic reccomendations. Note the use of the back tick to contain spaces in titles. ",
-    "the search command. `!MrLancer search` will return a fiction that matches the parameters given. No parameters results in a random fic from the list. ",
+    "At minimum, you must name the fic, and give the URL so that I can figure out how to group the fic recommendations. Note the use of the back tick to contain spaces in titles. ",
+    "The search command. `!MrLancer search` will return a fiction that matches the parameters given. No parameters results in a random fic from the list. ",
     "Meanwhile... ",
     " ```!MrLancer search genre angst``` ",
     "Returns a random angst ridden fic, as recommended by users such as yourself. ",
@@ -63,7 +63,7 @@ const helpText2 = [
 ].join('\n');
 
 const helpText3 = [
-    "`!MrLancer author` is used to sign up an an author as a prestigious member of our lists.",
+    "`!MrLancer author` is used to sign up an author as a prestigious member of our lists.",
     "You use it like so...",
     "```!MrLancer author `sign up` HeroineofTime https://archiveofourown.org/users/HeroineofTime/ ```",
     "If you cross post to two sites, include the second url as a second parameter.",
@@ -89,7 +89,11 @@ client.on("message", function (message) {
     const strings = message.content.split(/ (?=(?:[^\`]*\`[^\`]*\`)*[^\`]*$)/);
 
     //removes prefix from the strings array
-    strings.shift();
+    const prefix = strings.shift();
+
+    if(prefix.toLocaleLowerCase() === '!oldman'){
+        generateErrorReply(message, 'Who are you calling OLD man?');
+    }
 
     // if no arguements were passed, be done. 
     if(strings.length === 0){
